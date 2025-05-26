2e3bc83bcaee10c41e80476ca3a87398660e85d6
Karlo123-Byte <karlo.jozic@ekonomska.edu.ba>
2025-05-26 02:49:30 +0200

    logika

--- skripte/score.py
+++ skripte/score.py
@@ -1,14 +1,14 @@
 
 #polje za igru je 800x400
-#prvu zmiju treba stvroti na lijevoj strani
+#prvu zmiju treba stvroti na lijevoj strani 
 #drugoj zmiji treba stvoriti na desnoj strani
 
-#Prva zmija se treba kontrolirati na W,A,S,D
-#Druga zmija se treba kontrolirati na strelice
+#Prva zmija se treba kontrolirati na W,A,S,D Rijeio 
+#Druga zmija se treba kontrolirati na strelice Rijesio logiku
 
-#Prva zmija se treba stvoriti na 200,200 x,y
-#Druga zmija se treba stvoriti na 600,200 x,y
+#Prva zmija se treba stvoriti na 200,200 x,y Rijesio logiku
+#Druga zmija se treba stvoriti na 600,200 x,y Rijesio logiku
 
-#trebam uzeti sve slike od zmije i prebaciti u zutu boju da zmiji promijenim boju
+#trebam uzeti sve slike od zmije i prebaciti u zutu boju da zmiji promijenim boju  Rijesio
 
-#Dodati logiku ako se glava zmije sudari sa tijelom zmije ta zmija gubi i suprotno
+#Dodati logiku ako se glava zmije sudari sa tijelom zmije ta zmija gubi i suprotno 
