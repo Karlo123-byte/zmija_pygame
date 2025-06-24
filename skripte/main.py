ea0d5b316d340ae21312473b0e648080d77815eb
Karlo123-Byte <karlo.jozic@ekonomska.edu.ba>
2025-06-24 03:36:18 +0200

    Veliki update:
    - Dodana zuta zmija
    - Zuta se zmija krece na strelice
    - Zuta zmija jede jabuku
    - Ako se jedna zmija zabije u drugu ta druga je pobjedila
    - Ako se naprimjer zelena zamije u rub igrališta pisati će žuta zmija je pobijedila
    -Igra je spremna da se igra u dvoje

--- skripte/main.py
+++ skripte/main.py
@@ -3,6 +3,7 @@ import sys
 import random
 from zmija import Zmija
 from hrana import Hrana
+from zmija import ZutaZmija
 
 #inicijaliziranje pygamea, prozora, fontova
 pygame.init()
@@ -14,13 +15,13 @@ skalirana_slika = pygame.transform.scale(dno_pozadina, (800, 400))
 font = pygame.font.SysFont(None, 60)
 mali_font = pygame.font.SysFont(None, 30)
 
-
 SEGMENT_SIRINA = 10
 SEGMENT_VISINA = 10
 brzina = 5
-start_x = 200 #300 # početna pozicija zmije
-start_y = 200 #100 # početna pozicija zmije
-
+start_x = 200
+start_y = 200
+start_x1 = 400
+start_y1 = 200
 
 CRNA = (0, 0, 0)
 SIVA = (50, 50, 50)
@@ -35,11 +36,15 @@ def nova_pozicija_hrane():
 
 
 def reset_igre():
-    global zmija, hrana, dx, dy, game_over
+    global zmija, hrana, dx, dy, game_over, dx1, dy1, zmija1, pobjednik
+    pobjednik = ""
     pozicija_zmije = [(start_x - i * SEGMENT_SIRINA, start_y) for i in range(15)]
+    pozicija_zmije1 = [(start_x1 - i * SEGMENT_SIRINA, start_y1) for i in range(15)]
     zmija = Zmija(pozicija_zmije, SEGMENT_SIRINA, SEGMENT_VISINA)
+    zmija1 = ZutaZmija(pozicija_zmije1, SEGMENT_SIRINA, SEGMENT_VISINA)
     hrana = Hrana(nova_pozicija_hrane())
     dx, dy = brzina, 0
+    dx1, dy1 = brzina, 0
     game_over = False
 
 reset_igre()
@@ -51,7 +56,7 @@ while True:
             pygame.quit()
             sys.exit()
         if not game_over and event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w and dy == 0:
+            if event.key == pygame.K_w and dy == 0:        
                 dx, dy = 0, -brzina
             elif event.key == pygame.K_s and dy == 0:
                 dx, dy = 0, brzina
@@ -59,46 +64,76 @@ while True:
                 dx, dy = brzina, 0
             elif event.key == pygame.K_a and dx == 0:
                 dx, dy = -brzina, 0
+            elif event.key == pygame.K_UP and dy1 == 0:
+                dx1, dy1 = 0, -brzina
+            elif event.key == pygame.K_DOWN and dy1 == 0:
+                dx1, dy1 = 0, brzina
+            elif event.key == pygame.K_RIGHT and dx1 == 0:
+                dx1, dy1 = brzina, 0
+            elif event.key == pygame.K_LEFT and dx1 == 0:
+                dx1, dy1 = -brzina, 0            
         if game_over and event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 reset_igre()
+                
 
     if not game_over:
         zmija.pomakni(dx, dy)
+        zmija1.pomakni(dx1, dy1)
 
         glava_rect = pygame.Rect(zmija.pozicija[0][0], zmija.pozicija[0][1], 10, 10)
         hrana_rect = pygame.Rect(hrana.pozicija[0], hrana.pozicija[1], 15, 15)
+        glava_rect1 = pygame.Rect(zmija1.pozicija[0][0], zmija1.pozicija[0][1], 10, 10)
+        
 
         if zmija.game_over(zmija, zmija.pozicija):
             game_over = True
+            pobjednik = "ČESTITAMO! Žuta zmija je pobjedila!"
+
+        if zmija1.game_over(zmija1, zmija1.pozicija):
+            game_over = True    
+            pobjednik = "ČESTITAMO! Zelena zmija je pobjedila!"
 
         if glava_rect.colliderect(hrana_rect):
             zmija.rast_zmije(6)
             hrana.pozicija = nova_pozicija_hrane()
 
-    
+        if glava_rect1.colliderect(hrana_rect):
+            zmija1.rast_zmije(6)
+            hrana.pozicija = nova_pozicija_hrane()
+
+            # Sudar: plava glava u tijelo žute
+    if zmija.pozicija[0] in zmija1.pozicija[1:]:
+        game_over = True
+        pobjednik = "ČESTITAMO! Žuta zmija je pobjedila!"
+
+    # Sudar: žuta glava u tijelo plave
+    if zmija1.pozicija[0] in zmija.pozicija[1:]:
+        game_over = True
+        pobjednik = "ČESTITAMO! Zelena zmija je pobjedila!"        
+
+
     zaslon.blit(skalirana_slika, (0, 0))
     hrana.crtaj_hranu(zaslon)
     zmija.crtaj_zmiju(zaslon)
-    
+    zmija1.crtaj_zmiju(zaslon)
 
     if game_over:
-        
         overlay = pygame.Surface((800, 400), pygame.SRCALPHA)
         overlay.fill(OVERLAY)
         zaslon.blit(overlay, (0, 0))
 
-        
         popup = pygame.Rect(200, 80, 400, 240)
         pygame.draw.rect(zaslon, SIVA, popup)
         pygame.draw.rect(zaslon, BIJELA, popup, 5)
 
-        
         tekst = font.render("Game Over", True, BIJELA)
+        pobjednik_tekst = mali_font.render(pobjednik, True, BIJELA)
         podtekst = mali_font.render("Pritisni SPACE da pokušaš ponovno!", True, BIJELA)
 
-        zaslon.blit(tekst, (popup.centerx - tekst.get_width() // 2, popup.top + 50))
-        zaslon.blit(podtekst, (popup.centerx - podtekst.get_width() // 2, popup.top + 130))
+        zaslon.blit(tekst, (popup.centerx - tekst.get_width() // 2, popup.top + 30))
+        zaslon.blit(pobjednik_tekst, (popup.centerx - pobjednik_tekst.get_width() // 2, popup.top + 100))
+        zaslon.blit(podtekst, (popup.centerx - podtekst.get_width() // 2, popup.top + 170))
 
     pygame.display.update()
     sat.tick(60)
--- skripte/zmija.py
+++ skripte/zmija.py
@@ -98,5 +98,104 @@ class Zmija:
             return True
 
         return False
+    
+class ZutaZmija:
+    def __init__(self, pozicija, sirina, visina):
+        self.pozicija = pozicija  
+        self.segment_sirina = sirina
+        self.segment_visina = visina
 
+        self.glava = pygame.image.load("assets/slike/zuta_glava_zmije.png").convert_alpha()
+        self.tijelo = pygame.image.load("assets/slike/zuta_slika_tijela.png").convert_alpha()
+        self.rep = pygame.image.load("assets/slike/zuta_slika_repa.png").convert_alpha()
+
+        self.glava = pygame.transform.scale(self.glava, (self.segment_sirina * 2, self.segment_visina * 2))
+        self.tijelo = pygame.transform.scale(self.tijelo, (self.segment_sirina, self.segment_visina))
+        self.rep = pygame.transform.scale(self.rep, (self.segment_sirina, self.segment_visina))
+
+        self.smjerovi = [(1, 0)] * len(self.pozicija)
+
+    def rotiraj_sliku(self, slika, smjer):
+        if smjer == (0, -1):
+            return pygame.transform.rotate(slika, 180)
+        elif smjer == (1, 0):
+            return pygame.transform.rotate(slika, 90)
+        elif smjer == (0, 1):
+            return pygame.transform.rotate(slika, 0)
+        elif smjer == (-1, 0):
+            return pygame.transform.rotate(slika, 270)
+        return slika
+
+    def crtaj_zmiju(self, ekran):
+        glava_smjer = self.smjerovi[0]
+        glava_slika = self.rotiraj_sliku(self.glava, glava_smjer)
+        glava_rect = glava_slika.get_rect(center=(self.pozicija[0][0] + self.segment_sirina // 2,self.pozicija[0][1] + self.segment_visina // 2))
+                                           
+        ekran.blit(glava_slika, glava_rect)
+
+        for i in range(1, len(self.pozicija) - 1):
+            smjer = self.smjerovi[i]
+            tijelo_slika = self.rotiraj_sliku(self.tijelo, smjer)
+            ekran.blit(tijelo_slika, self.pozicija[i])
+
+        rep_smjer = self.smjerovi[-1]
+        rep_slika = self.rotiraj_sliku(self.rep, rep_smjer)
+        ekran.blit(rep_slika, self.pozicija[-1])
+
+    def pomakni(self, dx, dy):
+        nova_glava = (self.pozicija[0][0] + dx, self.pozicija[0][1] + dy)
+        self.pozicija = [nova_glava] + self.pozicija[:-1]
+
+        if dx > 0:
+            normirani_dx = 1
+        elif dx < 0:
+            normirani_dx = -1
+        else:
+            normirani_dx = 0
+
+        if dy > 0:
+            normirani_dy = 1
+        elif dy < 0:
+            normirani_dy = -1
+        else:
+            normirani_dy = 0
+
+        novi_smjer = (normirani_dx, normirani_dy)
+        stari_smjerovi = self.smjerovi[:-1]
+        self.smjerovi = [novi_smjer] + stari_smjerovi
+
+
+    def rast_zmije(self, broj):
+        for i in range(broj):
+            self.pozicija.append(self.pozicija[-1])
+            self.smjerovi.append(self.smjerovi[-1])
+
+    def pomakni_gore(self, brzina):
+        self.pomakni(0, -brzina)
+
+    def pomakni_dolje(self, brzina):
+        self.pomakni(0, brzina)
+
+    def pomakni_desno(self, brzina):
+        self.pomakni(brzina, 0)
+
+    def pomakni_lijevo(self, brzina):
+        self.pomakni(-brzina, 0)
+    
+    
+        
+    def game_over(self, zmija, pozicija):
+        glava = zmija.pozicija[0]
+
+        
+        if glava in zmija.pozicija[1:]:
+            return True
+
+        
+        x, y = glava
+        if x < 0 or x >= 800 or y < 0 or y >= 400:
+            return True
+
+        return False
+    
         
